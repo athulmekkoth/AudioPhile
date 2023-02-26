d8216428a953330de3341a880e7d650db96e44c6
Athul <athulmekkoth21@gmail.com>
2023-02-26 18:20:21 +0530

    redux

--- client/src/components/redux/store.js
+++ client/src/components/redux/store.js
@@ -1,8 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer  from './authslice.js'
 
-export default configureStore({
-    reducer: {
-        user: userReducer,
+import userReducer  from './authslice.js'
+import storage from 'redux-persist/lib/storage';
+
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { configureStore ,combineReducers} from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+
+
+
+  const rootReducer = combineReducers({ user: userReducer});
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
+    }),
 })
+export const persistor = persistStore(store)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import store from './components/redux/store.js'
+import {store,persistor} from './components/redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+ 
+<Provider store={store}>
+<PersistGate loading={null} persistor={persistor}>
   <App />
-</Provider>
+</PersistGate>
+</Provider>,
 )
-
 
 
