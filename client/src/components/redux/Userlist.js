c5f46a157e20384bf687ae5945ff67bfa4c7ab6a
Athul Suresh <athulmekkoth21@gmail.com>
2023-05-13 23:50:29 +0530

    layer

--- client/src/components/redux/Userlist.js
+++ client/src/components/redux/Userlist.js
@@ -1,5 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
+export const fetchData=createAsyncThunk(
+    'cart/fetchData',
+    async()=>{
+        try{
+      const response=await  axios.get("/api/auth/getuser")
+        return response.data
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 
+)
 export const list=createSlice({
     name:lsit,
     initialState:{
@@ -10,8 +22,30 @@ export const list=createSlice({
     error: null,
     },
     reducers:{
-        add(state,action)=>{
+      
+    }
+    ,
+    extraReducers:(builder)=>{
+        builder.
+        addCase(fetchData.pending,(state,action)=>{
+            state.pending=true;
+            state.fullfilled=false,
+            state.rejected=false;
 
-        }
+        })
+        addCase(fetchData.fullfilled,(state,action)=>{
+            state.pending=false;
+            state.fullfilled=false,
+            state.rejected=false;
+            if(action.payload!== undefined){
+            state.items=action.payload.response;
+            }
+
+        })
+        addCase(fetchData.rejected,(state,action)=>{
+            state.pending=false;
+            state.fullfilled=false,
+            state.rejected=true;
+        })
     }
 })
